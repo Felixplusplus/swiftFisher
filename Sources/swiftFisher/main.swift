9bd1090e3af873fbd36a075fea07ce0bf9fa2a9c
doubanjiang <doubanjiang@2dfire.com>
2019-03-05 16:31:31 +0800

    prepare rewrite

--- Sources/swiftFisher/main.swift
+++ Sources/swiftFisher/main.swift
@@ -6,16 +6,30 @@ import Rainbow
 
 
 let launchpath = FileManager.default.currentDirectoryPath
-print("======================================================")
-print("FX: script launch from \(launchpath)".yellow.underline)
-print("======================================================")
+//print("======================================================")
+//print("FX: script launch from \(launchpath)".yellow.underline)
+//print("======================================================")
+//
+//
+//let cmd = FXCommandLine()
+//cmd.addOptions(os: [gitStatusOption,gitBranchOption,gitOpenOption])
+//cmd.parseAll(args: CommandLine.arguments)
+//
+//exit(EX_OK)
 
 
-let cmd = FXCommandLine()
-cmd.addOptions(os: [gitStatusOption,gitBranchOption,gitOpenOption])
-cmd.parseAll(args: CommandLine.arguments)
-
-exit(EX_OK)
 
 
+import SwiftCLI
 
+class GreetCommand: Command {
+    let name = "greet"
+    let person = Parameter()
+    func execute() throws {
+        stdout <<< "Hello \(person.value.yellow)!"
+    }
+}
+
+let greeter = CLI(name: "greeter")
+greeter.commands = [GreetCommand()]
+_ = greeter.go()
